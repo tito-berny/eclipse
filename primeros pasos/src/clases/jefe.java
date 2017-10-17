129ce95b42adefbaf2e6dc65fcdb0f801d3b6c16
berny <berny_tito@hotmail.com>
2017-10-17 15:45:35 +0200

    incremento jefe

---
+++ primeros pasos/src/clases/jefe.java
@@ -0,0 +1,32 @@
+package clases;
+
+public class jefe {
+	
+	private String carreg;
+	private double incremento;
+	public String getCarreg() {
+		return carreg;
+	}
+	public void setCarreg(String carreg) {
+		this.carreg = carreg;
+	}
+	public double getIncremento() {
+		return incremento;
+	}
+	public void setIncremento(double incremento) {
+		this.incremento = incremento;
+	}
+	public jefe() {
+		super();
+	}
+	
+	public jefe(String carreg, double incremento) {
+		super();
+		this.carreg = carreg;
+		this.incremento = incremento;
+	}
+	
+	
+	
+
+}
